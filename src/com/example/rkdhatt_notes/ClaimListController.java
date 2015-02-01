9e678b1ffd3eec5f929821fffc4eda9683bba43e
rkdhatt <rkdhatt@ualberta.ca>
2015-01-31 17:33:26 -0700

    Described classes and activities via licenses

--- src/com/example/rkdhatt_notes/ClaimListController.java
+++ src/com/example/rkdhatt_notes/ClaimListController.java
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
 
-// Source: https://www.youtube.com/watch?v=uLnoI7mbuEo
+// Source: https://www.youtube.com/watch?v=uLnoI7mbuEo 2015-01-06
 package com.example.rkdhatt_notes;
 
 import java.io.IOException;
--- src/com/example/rkdhatt_notes/DeleteClaimActivity.java
+++ src/com/example/rkdhatt_notes/DeleteClaimActivity.java
@@ -1,3 +1,22 @@
+/* An activity for deleting a selected claim and updates corresponding list view
+ * 
+ * 
+ * 
+ * Copyright (C) 2015 Ramandeep Dhatt rkdhatt@ulberta.ca
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.*/
+
 package com.example.rkdhatt_notes;
 
 import java.util.ArrayList;
@@ -91,9 +110,6 @@ public class DeleteClaimActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
